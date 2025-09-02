fba3fc353b3d448e16a16a2c537b73eed162deff
Hans Peter Diesing <hans.diesing@netigo.de>
2025-09-02 20:52:19 +0200

    feat: add Homebrew formula for easy installation

---
+++ Formula/aic.rb
@@ -0,0 +1,20 @@
+class Aic < Formula
+  desc "AI-assisted git commit message generator"
+  homepage "https://github.com/Hans2711/aic"
+  url "https://github.com/Hans2711/aic/archive/dad708eb1004cf24954193adca38cbc00f26aee0.tar.gz"
+  sha256 "70577b15d7c764de9905895da1583c0750ace9aff6bbb2e9adca1319dcc39c5d"
+  version "1.0.0"
+  head "https://github.com/Hans2711/aic.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/aic"
+  end
+
+  test do
+    out = shell_output("#{bin}/aic --version")
+    assert_match "aic #{version}", out
+  end
+end
+
