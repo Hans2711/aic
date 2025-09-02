60faf7cbb63039552504126709c2eb09c32d7e14
Hans Peter Diesing <hans.diesing@netigo.de>
2025-09-03 00:12:17 +0200

    Bump version to 1.0.8 in version.go file.

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,6 +2,6 @@ package version
 
 // Version is a static label for this project.
 // Per project policy, set explicitly and not overridden at build time.
-const Version = "1.0.7"
+const Version = "1.0.8"
 
 func Get() string { return Version }
