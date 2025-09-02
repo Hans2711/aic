b621d49805e1c98f9c4a3913d7b85a8d9ebf92b1
Hans Peter Diesing <hans.diesing@netigo.de>
2025-09-02 23:34:01 +0200

    Update version to 1.0.6 in version.go file.

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,6 +2,6 @@ package version
 
 // Version is a static label for this project.
 // Per project policy, set explicitly and not overridden at build time.
-const Version = "1.0.0"
+const Version = "1.0.6"
 
 func Get() string { return Version }
